85e980695683f82f9103980f04fc96bb7a4e2d8d
kibet <kibet@kibet.edu>
2024-11-10 07:59:41 +0300

    Buddy is a bulldog

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
-fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-  print(x)
-  if x == "banana":
-    break
+class Dog:
+  
+  def __init__(self, name, breed):
+    self.name = name
+    self.breed = breed
+
+  def __str__(self):
+    return f"{self.name} is a {self.breed}."
+
+dog = Dog("Buddy", "Bulldog")
+print(dog)   
